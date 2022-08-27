93f6c3555f490993aedda7c164c9b8fe142d55d7
mehedicse133 <mehedicse047@gmail.com>
2022-08-27 19:10:09 +0600

    add delete_last method

--- singly_link_list.py
+++ singly_link_list.py
@@ -17,22 +17,34 @@ class Singly_link_list:
             self.head = new_node
         else:
             new_node.next = self.head
-            self.head = new_node       
+            self.head = new_node   
+
     def peak_last(self):
         temp = self.head
         while temp:
             if temp.next == None:
                 return print(temp.data)
             temp = temp.next
+              
+    def peak_head(self):
+        return print(self.head.data) 
+    
     def delete_first(self):
         if self.head == None:
             return
         else:
             temp = self.head
-            self.head = temp.next            
+            self.head = temp.next
 
-    def peak_head(self):
-        return print(self.head.data)  
+    def delete_last(self):
+        temp = self.head
+        c = 0
+        while temp:
+            c += 1
+            temp = temp.next
+        print(c)
+
+
 
     def display_list(self):
         temp = self.head
@@ -52,14 +64,18 @@ if __name__ == "__main__":
    
     l.display_list()
     print('-'*20)
+    l.delete_last()
+
     # print('-'*20)
     # l.peak_head()
     # print('-'*20)
     # l.peak_last()
-    l.delete_first()
-    l.display_list()
-    print('-'*20)
-    l.display_list()
-    print('-'*20)
-    l.delete_first()
-    l.display_list()
+
+
+    # l.delete_first()
+    # l.display_list()
+    # print('-'*20)
+    # l.display_list()
+    # print('-'*20)
+    # l.delete_first()
+    # l.display_list()
