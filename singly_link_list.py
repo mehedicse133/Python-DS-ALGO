347be04154e59f474916b8cb38c67cea8db60e22
mehedicse133 <mehedicse047@gmail.com>
2022-08-27 16:54:11 +0600

    add peak_head method

--- singly_link_list.py
+++ singly_link_list.py
@@ -1,4 +1,6 @@
 # Python Singly link list
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -17,15 +19,19 @@ class Singly_link_list:
             new_node.next = self.head
             self.head = new_node
 
+    def peak_head(self):
+        return print(self.head.data)
+
     def display_list(self):
         temp = self.head
         while temp is not None:
             print(temp.data)
             temp = temp.next
 
-
-l = Singly_link_list()
-l.add_first(20)
-l.add_first(23)
-l.add_first(10)
-l.display_list()
+if __name__ == "__main__":
+    l = Singly_link_list()
+    l.add_first(20)
+    l.add_first(23)
+    l.add_first(10)
+    l.display_list()
+    l.peak_head()
