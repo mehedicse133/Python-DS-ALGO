5b41fde21f488fb009ec0d6e124f76402cf423d2
mehedicse133 <mehedicse047@gmail.com>
2022-09-10 19:07:17 +0600

    implemenet and method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,51 +1,68 @@
-# Implement doubly link list
+# Python Doubly linked list
 
-class Node:
+class node:
     def __init__(self,data):
         self.data = data
         self.next = None
         self.prev = None
 
-class doubly_Llist:
+class doublylist:
     def __init__(self):
         self.head = None
         self.tail = None
 
-    def add(self,data): 
-        new = Node(data) 
-        if self.head == None:         
-            self.head = new
-            self.tail = new
+    def add_at_first(self,data):
+        new_node = node(data)            
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+            
         else:
-            pass
-        
-    def add_last(self,data):
-        pass
+            
+            new_node.next = self.head
+            self.head = new_node
+            self.tail = new_node.next
+
+           
+            
+
+            # new_node.next = self.head
+            # self.head.prev = new_node
+            # self.tail = new_node.next
+            # # self.tail.prev = new_node
+            # self.head = new_node
+
+    def display(self):
+        temp = self.head          
+        while temp is not None:
+            print(temp.data)
+            temp = temp.next
+
+    def peak_first(self):
+        return self.head
+
+    def peak_last(self):
+        return self.tail
+
+    def reverse(self):
+        tail = self.tail
+        while tail.prev is not None:
+            print(tail.prev.data)
+            tail = tail.prev
+
 
 
 if __name__ == "__main__":
-
-
-    node1 = Node(10)
-    node2 = Node(20)
-    node3 = Node(30)
-    list = doubly_Llist()
-
-    list.head = node1
-    list.tail = node1
-
-    node1.prev =list.head.prev
-
-    node1.next =node2
-    list.tail = node2
-    print(list.tail.data)
-
-    node2.next = node3
-    node2.prev = node2
-    list.tail =node3
-    node3.prev = node2
-    print(list.tail.data)
-    print(list.tail.prev.data)
+    l = doublylist() 
+    l.add_at_first(21)          
+    l.add_at_first(27)          
+    l.add_at_first(88)          
+    l.add_at_first(11)          
+    l.add_at_first(66)          
+            
+    l.display()
+ 
+  
    
 
     
