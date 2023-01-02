19f640f8527f410b4ae55dd8c2c115b8ee741db8
mehedicse133 <mehedicse047@gmail.com>
2023-01-02 11:48:14 +0600

    add oops approaach

--- queue_implement_with_list.py
+++ queue_implement_with_list.py
@@ -19,11 +19,54 @@ def is_empty():
         return  True
     return False
 
-enqueue(20)            
-enqueue(27)            
-enqueue(22)
-print(queue)
-length()  
-dequeue() 
-print(queue) 
-length()        
+# enqueue(20)            
+# enqueue(27)            
+# enqueue(22)
+# print(queue)
+# length()  
+# dequeue() 
+# print(queue) 
+# length() 
+
+# Object oriented approach
+class Queue:
+    def __init__(self) -> None:
+        self.queue = []
+    
+
+    def enqueue(self,val):
+         self.queue.append(val)
+
+
+    def dequeue(self):
+        val =  self.queue.pop(0)
+        print(val)
+
+    def front_value(self):
+        if is_empty:
+            print("queue is empty")
+        else:    
+            return  self.queue[0]
+
+    def print_queue(self):
+        print( self.queue)
+
+    def length(self):
+        return len( self.queue) 
+
+    def is_empty(self):
+        if len(self.queue) == -1: 
+            return  True
+        return False 
+    
+
+if __name__ == "__main__":
+    q = Queue()
+    # s.peek()
+    q.enqueue(44)
+    q.enqueue(88)
+    q.print_queue()
+    q.dequeue()
+    q.print_queue()
+
+  
